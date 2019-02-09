92d1ea2ed7fb3052122a69d0aeb0e610de0d7636
uriegel <uriegel@hotmail.de>
2019-02-09 22:27:09 +0100

    Add project files.

---
+++ Tester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ULog;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new Logger();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
