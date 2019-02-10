a09c312f18674ebede18ab02706808306666c8b3
uriegel <uriegel@hotmail.de>
2019-02-10 12:44:05 +0100

    WindowsTraceSwitcher and WindowsService, trace switching not working

--- TestLogService/LogService.cs
+++ TestLogService/LogService.cs
@@ -1,26 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using ULog;
+using ULogWin;
 
-namespace Tester
+namespace TestLogService
 {
-    class Program
+    public partial class LogService : ServiceBase
     {
-        static void Main(string[] args)
-        {
-            (new Thread(() =>
-            {
-                Console.ReadLine();
-                stopEvent.Set();
-            })).Start();
+        public LogService() => InitializeComponent();
 
+        protected override void OnStart(string[] args) => (new Thread(Run)).Start();
+
+        protected override void OnStop() => stopEvent.Set();
+
+        void Run()
+        {
             using (var logger = new Logger(new LogSettings
             {
                 Category = "Main",
-                LogFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "LogTest", "test.log"),
-                TraceKindConsole = TraceKind.Verbose
-            }, new TraceControl()))
+                LogFile = Path.Combine(@"C:\ProgramData", "LogTest", "test.log")
+            }, new WindowsTraceControl()))
             {
                 logger.Info("=====================================");
                 logger.Trace(() => "running...");
@@ -35,7 +43,9 @@ namespace Tester
                         break;
                 }
             }
+
         }
+
         static ManualResetEvent stopEvent = new ManualResetEvent(false);
     }
 }
---
+++ WindowsTraceSwitcher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ULog;
+using ULogWin;
+
+namespace WindowsTraceSwitcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var control = new WindowsTraceControl();
+            while (true)
+            {
+                Console.WriteLine("type 'trace' or 'verbose' or 'none'");
+                var kind = Console.ReadLine();
+                switch (kind)
+                {
+                    case "trace":
+                        control.SetKind(TraceKind.Trace);
+                        break;
+                    case "verbose":
+                        control.SetKind(TraceKind.Verbose);
+                        break;
+                    default:
+                        control.SetKind(TraceKind.None);
+                        break;
+                }
+            }
+        }
+    }
+}
